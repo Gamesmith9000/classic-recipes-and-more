a0a6d47e4111086e88c6625c47f05085c15ef82a
Julian Smith <smith.julian.f@gmail.com>
2020-06-16 08:59:44 -0500

    Added React router, set up Rails and React routers to work in tandem, and added first two pages

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
 
 class App extends React.Component {
     render () {
         return (
-            <div>APP COMPONENT</div>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                </Switch>
+            </div>
         );
     }
 }
