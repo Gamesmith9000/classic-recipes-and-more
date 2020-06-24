ca8c21e6ee992cd7da0b7588a624566fb7e0c599
Julian Smith <smith.julian.f@gmail.com>
2020-06-24 14:13:48 -0500

    Created form and logic for admin login. Not currently persistent

--- app/javascript/components/AdminLogin.js
+++ app/javascript/components/AdminLogin.js
@@ -1,12 +1,41 @@
 import React from 'react'
 import Navbar from './Navbar'
+import firebaseApp from 'FirebaseInit'
 
 class AdminLogin extends React.Component {
+    submitCredentials = (event) => {
+        firebaseApp.auth().signInWithEmailAndPassword(this.email.value, this.password.value)
+        .then( (res) => console.log('Success. You logged in.'))
+        .catch((err) => console.log('Error. Login not successful'));
+
+        event.preventDefault();
+    }
     render() {
         return (
             <div className="admin-login">
                 <Navbar />
                 Admin Login
+                <div className="login-section">
+                    <h1>Sign In</h1>
+                    <form onSubmit={this.submitCredentials} className="login-form">
+                        <label>Email</label>
+                        <input
+                            placeholder="name@example.com"
+                            type="email"
+                            ref={input => this.email = input}
+                        />
+                        <label>Password</label>
+                        <input 
+                            type="password"
+                            ref={input => this.password = input}    
+                        />
+                        <input 
+                            type="submit"
+                            onSubmit={this.submitCredentials}
+                        />
+                    </form>
+                    
+                </div>
             </div>
         )
     }
