ad7e94ade7b695402e89be390dc6ecd168a69400
Julian Smith <smith.julian.f@gmail.com>
2020-08-26 16:23:50 -0500

    Dialed back aspects to be devloped later: admin, firebase, and CMS

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -3,8 +3,6 @@ import { Route, Switch } from 'react-router-dom'
 
 import Home from './Pages/Home'
 import About from './Pages/About'
-import AdminLogin from './Pages/AdminLogin'
-import Content from './Pages/Content'
 import Cookbook from './Pages/Cookbook'
 import CookingVideos from './Pages/CookingVideos'
 import SeasonalRecipes from './Pages/SeasonalRecipes'
@@ -17,8 +15,6 @@ class App extends React.Component {
                     <Route exact path="/" component={Home} />
                     <Route exact path="/about" component={About} />
                     <Route exact path="/cookbook" component={Cookbook} />
-                    <Route exact path="/content" component={Content} />
-                    <Route exact path="/content/login" component={AdminLogin} />
                     <Route exact path="/seasonal" component={SeasonalRecipes} />
                     <Route exact path="/videos" component={CookingVideos} />
                 </Switch>
