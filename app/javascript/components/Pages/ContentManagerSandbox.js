399f8961cb29f41e164dfc25caa7454697052fba
Julian Smith <smith.julian.f@gmail.com>
2020-09-02 13:21:34 -0500

    Refactored the image upload form in the sandbox component

--- app/javascript/components/Pages/ContentManagerSandbox.js
+++ app/javascript/components/Pages/ContentManagerSandbox.js
@@ -1,52 +1,78 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import axios from 'axios'
 
 class ContentManagerSandbox extends React.Component {
-    constructor() {
-        super();
-        this.imageFileInput = React.createRef();
-        this.titleInput = React.createRef();
-        this.notesInput = React.createRef();
+    constructor(props) {
+        super(props);
+        this.state = {
+            photoFile: null,
+            photoTitle: null,
+            photoNotes: null
+        }
     }
 
-    handleSubmit = (event) => {
+    handlePhotoUploadSubmit = (event) => {
         event.preventDefault();
-        console.log(this.imageFileInput.current.files[0]);
-        console.log(this.titleInput.current.value);
-        console.log(this.notesInput.current.value);
+        console.log(this.state.photoFile);
+        console.log(this.state.photoTitle);
+        console.log(this.state.photoNotes);
 
         const csrfToken = document.querySelector('meta[name=csrf-token]').content;
         axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
 
         axios.post('api/v1/photos', {
-            file: this.imageFileInput.current.files[0],
-            title: this.titleInput.current.value,
-            notes: this.notesInput.current.value
+            file:  this.state.photoFile,
+            title: this.state.photoTitle,
+            notes: this.state.photoNotes
         })
         .then(res => console.log(res))
         .catch(err => console.log(err));
     }
 
+    onPhotoFileInputChange = (event) => {
+        console.log(event.target.files);
+        console.log(event.target.files[0]);
+
+        this.setState({photoFile: event.target.files[0]})
+    }
+
+    onPhotoTitleInputChange = (event) => {
+        this.setState({photoTitle: event.target.value})
+    }
+    
+    onPhotoNotesInputChange = (event) => {
+        this.setState({photoNotes: event.target.value})
+    }
+
     render() {
+        let imageUploadForm = <Fragment>
+            <hr/>
+            <form onSubmit={this.handlePhotoUploadSubmit}>
+                <h4>Photo Uploader</h4>
+                <label>
+                    Image
+                    <input type="file" onChange={this.onPhotoFileInputChange} />
+                </label>
+                <br/>
+                <label>
+                    Title
+                    <input type="text" onChange={this.onPhotoTitleInputChange} />
+                </label>
+                <br/>
+                <label>
+                    Notes
+                    <input type="text" onChange={this.onPhotoNotesInputChange} />
+                </label>
+                <br/>
+                <button type="submit">Upload</button>
+            </form>
+            <hr/>
+        </Fragment>
+
         return (
             <div className="content-manager-sandbox">
                 <p>[ContentManagerSandbox Component]</p>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Image uploader example
-                        <input type="file" ref={this.imageFileInput} />
-                    </label>
-                    <label>
-                        Title
-                        <input type="text" ref={this.titleInput} />
-                    </label>
-                    <label>
-                        Notes
-                        <input type="text" ref={this.notesInput} />
-                    </label>
-                    <br/>
-                    <button type="submit">Upload</button>
-                </form>
+                {imageUploadForm}
             </div>
         )
     }
