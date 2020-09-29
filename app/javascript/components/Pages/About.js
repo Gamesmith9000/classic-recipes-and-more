dc80a644c2b2f6773fb426f1af4c63d4489e5aa2
Julian Smith <smith.julian.f@gmail.com>
2020-09-29 10:46:07 -0500

    Added loading and rendering of 'about paragraphs' in About component

--- app/javascript/components/Pages/About.js
+++ app/javascript/components/Pages/About.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import axios from 'axios'
 
 class About extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            paragraphs: []
+        }
+    }
+
+    componentDidMount () {
+        axios.get('/api/v1/aux/main.json')
+        .then(res => {
+            this.setState({
+                paragraphs: res.data.data.attributes.about_page_paragraphs,
+            });
+        })
+        .catch(err => console.log(err));
+    }
+
+    mapParagraphs = (paragraphsList) => {
+        return paragraphsList.map((item, index) => {
+            return (
+                <p key={index}>
+                    {item}
+                </p>
+            )
+        });
+    }
+
     render() {
         return (
             <div className="about">
-                <p>[About Component]</p>
+                <h1>About Us</h1>
+                <div className="about-story">
+                    {this.mapParagraphs(this.state.paragraphs)}
+                </div>
             </div>
         )
     }
