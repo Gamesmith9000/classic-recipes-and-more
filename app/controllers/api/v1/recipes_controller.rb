395f063433a55a16fd0249d23206967c923c3546
Julian Smith <smith.julian.f@gmail.com>
2020-10-08 12:36:23 -0500

    In recipies_controller#update, got updates working when there are more/less sections than prior to update

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -36,18 +36,54 @@ module Api
             def update
                 recipe = Recipe.find_by_id(params[:id])
 
-                if Recipe.update(recipe_params)
+                # [NOTE] Focusing on getting this working properly. Refactoring & more DRY approach after
+
+                if recipe.update(recipe_params)
                     if params.has_key? :sections
                         prior_sections_data = Section.where(recipe_id: params[:id]).order('created_at DESC').reverse_order.as_json
                         new_sections_data = params[:sections]
 
-                        # The following will successfully update items, but will not work if a new item is added or prior item is deleted
+                        if new_sections_data.length == prior_sections_data.length
+                            prior_sections_data.each_with_index do |data, index|
+                                new_data = new_sections_data[index]
+                                section_id = prior_sections_data[index][:id]
 
-                        prior_sections_data.each_with_index do |data, index|
-                            new_data = new_sections_data[index]
-                            section_id = prior_sections_data[index][:id]
+                                Section.update(new_data[:id], :text_content => new_data[:text_content], :ordered_photo_ids => new_data[:ordered_photo_ids])
+                            end
+                        else
+                            length_variance = (new_sections_data.length - prior_sections_data.length).abs
 
-                            Section.update(new_data[:id], :text_content => new_data[:text_content], :ordered_photo_ids => new_data[:ordered_photo_ids])
+                            if new_sections_data.length > prior_sections_data.length
+                                # if (1+) new piece of section data was added via form
+
+                                prior_sections_data.each_with_index do |data, index| # copied from above, but with added steps:
+                                    new_data = new_sections_data[index]
+                                    section_id = prior_sections_data[index][:id]
+    
+                                    Section.update(new_data[:id], :text_content => new_data[:text_content], :ordered_photo_ids => new_data[:ordered_photo_ids])
+                                end
+
+                                length_variance.times do |i|
+                                    new_data = new_sections_data[prior_sections_data.length + i]
+                                    
+                                    Section.create(:recipe_id => params[:id], :text_content => new_data[:text_content], :ordered_photo_ids => new_data[:ordered_photo_ids])
+                                end
+                            else
+                                # if (1+) section data entry was deleted via form
+
+                                new_sections_data.each_with_index do |data, index|
+                                    new_data = new_sections_data[index]
+                                    section_id = prior_sections_data[index][:id]
+    
+                                    Section.update(new_data[:id], :text_content => new_data[:text_content], :ordered_photo_ids => new_data[:ordered_photo_ids])
+                                end
+
+                                length_variance.times do |i|
+                                    prior_data = prior_sections_data[new_sections_data.length + i]
+
+                                    Section.destroy(prior_data["id"])
+                                end
+                            end
                         end
                     end
 
