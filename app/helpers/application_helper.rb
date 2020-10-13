16e91fa815ca9d22cff7a49ed6ccc90a735288ab
Julian Smith <smith.julian.f@gmail.com>
2020-10-12 21:40:51 -0500

    Fixed font-import issue by putting stylesheet link in application.html.erb. Tidied some associated items.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
     def successful_sign_in_message
+        # [NOTE] This string should be loaded dynamically. As is, this functionality will break
+        # if Devise succesful sign-in message is customized, or if locale is changed
         return 'Signed in successfully.'
     end
 end
