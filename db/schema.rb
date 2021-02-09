c3ce6cc16477e898283f1bd766ef9c2739627f1b
Julian Smith <smith.julian.f@gmail.com>
2021-02-09 11:13:44 -0600

    Removed unused items from db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_01_023725) do
+ActiveRecord::Schema.define(version: 2021_02_09_170823) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2021_02_01_023725) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "about_page_sections", default: [""], array: true
-    t.string "photo_tags", default: ["DEFAULT"], array: true
   end
 
   create_table "instructions", force: :cascade do |t|
@@ -60,14 +59,6 @@ ActiveRecord::Schema.define(version: 2021_02_01_023725) do
     t.boolean "featured", default: false
     t.string "description", default: ""
     t.integer "photo_id"
-  end
-
-  create_table "sections", force: :cascade do |t|
-    t.integer "ordered_photo_ids", array: true
-    t.integer "recipe_id"
-    t.text "text_content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
