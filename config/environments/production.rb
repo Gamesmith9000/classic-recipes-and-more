a910eabf99a5234c1e05a2b0a0043c70b95c9af9
Julian Smith <smith.julian.f@gmail.com>
2021-02-10 11:34:17 -0600

    Renamed remaining items from 'Cooking Classics Anywhere' to 'Classic Recipes and More'

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "cooking_classics_anywhere_production"
+  # config.active_job.queue_name_prefix = "classic_recipes_and_more_production"
 
   config.action_mailer.perform_caching = false
 
